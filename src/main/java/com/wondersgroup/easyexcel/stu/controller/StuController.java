32327acb299e545ffb6ee3ba538f3444e60484e4
lizhu <lzlj21@163.com>
2020-12-01 15:40:16 +0800

    整理swagger2文档工具

--- src/main/java/com/wondersgroup/easyexcel/stu/controller/StuController.java
+++ src/main/java/com/wondersgroup/easyexcel/stu/controller/StuController.java
@@ -2,6 +2,8 @@ package com.wondersgroup.easyexcel.stu.controller;
 
 import com.wondersgroup.easyexcel.stu.pojo.Stu;
 import com.wondersgroup.easyexcel.stu.service.StuService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import jdk.nashorn.internal.objects.annotations.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +17,7 @@ import java.util.List;
  * @date: 2020/12/1 14:56
  * @description:
  */
+@Api(value = "测试接口",tags = {"用户测试相关的接口"})
 @RestController
 @RequestMapping("stu")
 public class StuController {
@@ -22,6 +25,7 @@ public class StuController {
     @Autowired
     private StuService stuService;
 
+    @ApiOperation(value = "查询全部数据",notes = "查询全部数据",httpMethod = "GET")
     @GetMapping("query")
     public List<Stu> query(){
         List<Stu> stus = stuService.query();
--- src/main/java/com/wondersgroup/easyexcel/stu/pojo/Stu.java
+++ src/main/java/com/wondersgroup/easyexcel/stu/pojo/Stu.java
@@ -1,13 +1,20 @@
 package com.wondersgroup.easyexcel.stu.pojo;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.persistence.Id;
 
+@ApiModel(value = "测试实例",description = "测试实例")
 public class Stu {
     @Id
+    @ApiModelProperty(value = "ID",name = "ID")
     private Integer id;
 
+    @ApiModelProperty(value = "名字",name = "名字")
     private String name;
 
+    @ApiModelProperty(value ="年龄",name = "年龄")
     private Integer age;
 
     /**
